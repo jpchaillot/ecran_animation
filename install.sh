d88e25c20ce29a18b682eb5e42c73b2ef0fd4858
jpchaillot <jpchaillot@gmail.com>
2019-09-22 23:14:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,22 @@ sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install aptitude -y
 sudo apt-get install youtube-dl -y
+# mettre a jour youtube dl
+sudo youtube-dl -U
 
 sudo aptitude install apache2 -y
 
 sudo chown -R pi:www-data /var/www/html/
 sudo chmod -R 770 /var/www/html/
+sudo mkdir /var/www/html/video
+sudo chmod -R 770 /var/www/html/video
+
 
 sudo aptitude install php -y
 
+wget -P /var/www/html https://raw.githubusercontent.com/jpchaillot/ecran_animation/master/envoi.php
+wget -P /var/www/html https://raw.githubusercontent.com/jpchaillot/ecran_animation/master/index.php
+wget -P /var/www/html https://raw.githubusercontent.com/jpchaillot/ecran_animation/master/suppression.php
 
-#ensuite installation de composer et avec installation de youtube-dl-php de norkumas
-curl -sS https://getcomposer.org/installer | php
-php composer.phar require norkunas/youtube-dl-php
+sudo git clone https://github.com/adafruit/pi_video_looper.git /home/pi/pi_video_looper/
+sudo ./home/pi/pi_video_looper/install.shrm 
