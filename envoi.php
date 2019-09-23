cd095c0db9a515b7e60cd9f1b7355a282f6c6af1
jpchaillot <jpchaillot@gmail.com>
2019-09-23 22:17:43 +0200

    Update envoi.php

--- envoi.php
+++ envoi.php
@@ -6,7 +6,7 @@ if(!isset($_POST['video_url'])){
     header('Location: index.php');
 }
 
-shell_exec("youtube-dl -o '/var/www/html/video/%(title)s.%(ext)s' ".$_POST['video_url']);
+shell_exec("youtube-dl -o -f best '/var/www/html/video/%(title)s.%(ext)s' ".$_POST['video_url']);
 
 
 //var_dump($_POST);
